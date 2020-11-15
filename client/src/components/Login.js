7c0c48b57ff543094939c8d3abdc3ec676d91a5d
Kevin Luo <kvn.luo@gmail.com>
2020-11-15 16:59:12 -0500

    google oauth clientid

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { GraphQLClient } from 'graphql-request';
+import { GoogleLogin } from 'react-google-login';
 
 export default function Login() {
+  
+
   return (
-    <div>
-      
-    </div>
+    <GoogleLogin 
+      clientId={ process.env.REACT_APP_OAUTH_CLIENT_ID }
+      onSuccess={ onSuccess }
+      isSignedIn={ true }
+    />
   )
 }
