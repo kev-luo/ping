b79bafeae43687008459460111c51a282a7fe8e2
JD Martinez <focus4ursoul@gmail.com>
2020-11-15 13:26:19 -0800

    add base type definitions for queries

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,39 +1,54 @@
 const { gql } = require("apollo-server");
 
 module.exports = gql`
-
 type Ping {
     id: ID!
-    body: String 
+    body: String!
+    createdAt: String!
+    username: String! 
+    comments: [Comment]!
+    support: [Support]!
+    supportCount: Int!
+    commentCount: Int! 
 }
-
 type Comment {
-
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
 }
-
 type Support {
-
+    id: ID!
+    createdAt: String!
+    username: String!
 }
-
 type User {
-
+    id: ID!
+    email: String!
+    token: String!
+    username: String!
+    createdAt: String!
 }
-
 input RegisterInput {
-
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
 }
-
 type Query {
-
+    getPings: [Ping]
+    getPings(pingId: ID!): Ping
 }
-
 type Mutation {
-
+    register(registerInput: RegisterInput): User!
+    login(username: String!, password: String!): User!
+    createPing(body: String!): Ping!
+    deletePing(pingID: ID!): String!
+    createComment(pingId: ID!, body: String!): Ping!
+    deleteComment(pingId: ID!, commentId: ID!): Ping!
+    supportPing(pingId: ID!): Ping!
 }
-
 type Subscription {
-
+    newPing: Ping!
 }
-
-
 `
