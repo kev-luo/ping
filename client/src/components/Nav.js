0529df9ed95817cf18fe226e067ad73499808588
Kevin Luo <kvn.luo@gmail.com>
2020-11-16 11:53:05 -0500

    feed styling

---
+++ client/src/components/Nav.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper, AppBar, Toolbar, Typography, InputBase, fade } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    flexGrow: 1,
+    color: theme.palette.grey[400]
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    width: '20%'
+  },
+  searchIcon: {
+    position: 'absolute',
+    padding: theme.spacing(0,2),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%'
+  },
+  input: {
+    paddingLeft: '3em',
+    color: 'inherit',
+  }
+}))
+
+export default function Nav() {
+  const classes = useStyles();
+  return (
+    <Paper elevation={3}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4" className={classes.title}>
+            Ping
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon/>
+            </div>
+            <InputBase placeholder="Search..." className={classes.input}/>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Paper>
+  )
+}
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,53 +1,52 @@
 import React from 'react';
+import { Grid, Paper, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, AppBar, Toolbar, Typography, InputBase, fade } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
+
+import Nav from '../components/Nav';
+import Feed from '../components/Feed';
 
 const useStyles = makeStyles(theme => ({
-  title: {
+  root: {
     flexGrow: 1,
-    color: theme.palette.grey[400]
+    margin: theme.spacing(1,1)
   },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    width: '20%'
+  grid: {
+    margin: theme.spacing(1,1)
   },
-  searchIcon: {
-    position: 'absolute',
-    padding: theme.spacing(0,2),
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%'
-  },
-  input: {
-    paddingLeft: '3em',
-    color: 'inherit',
+  paper: {
+    padding: theme.spacing(2),
+    // color: theme.palette.text.secondary,
   }
 }))
 
 export default function Dashboard() {
   const classes = useStyles();
   return (
-    <Paper elevation={3}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h4" className={classes.title}>
-            Ping
-          </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon/>
-            </div>
-            <InputBase placeholder="Search..." className={classes.input}/>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </Paper>
+    <div className={classes.root}>
+      <Nav />
+      <div className={classes.grid}>
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>
+              <Typography>
+                ProfileProfileProfileProfileProfileProfileProfileProfileProfile
+                ProProfileProfileProfileProfileProfileProfileProfileProfileProfilefile
+                ProfProfileProfileProfileProfileProfileProfileProfileProfileProfileile
+                ProProfileProfileProfileProfileProfileProfileProfileProfileProfilefile
+                PrProfileProfileProfileProfileProfileProfileProfileProfileProfileofile
+                PrProfileProfileProfileProfileProfileProfileProfileProfileProfileofile
+                PrProfileProfileProfileProfileProfileProfileProfileProfileProfileofile
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={8}>
+            <Paper className={classes.paper}><Feed /></Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>Map</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
   )
 }
