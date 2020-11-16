01ecba71296b1daeebbf2eee11bc8158b2fd85c2
Kevin Luo <kvn.luo@gmail.com>
2020-11-16 11:13:57 -0500

    nav styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
+import React from 'react';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Dashboard />
   );
 }
 
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -5,14 +5,29 @@ import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles(theme => ({
   title: {
-    flexGrow: 1
+    flexGrow: 1,
+    color: theme.palette.grey[400]
   },
   search: {
+    position: 'relative',
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
     '&:hover': {
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
+    width: '20%'
+  },
+  searchIcon: {
+    position: 'absolute',
+    padding: theme.spacing(0,2),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%'
+  },
+  input: {
+    paddingLeft: '3em',
+    color: 'inherit',
   }
 }))
 
@@ -26,8 +41,10 @@ export default function Dashboard() {
             Ping
           </Typography>
           <div className={classes.search}>
-            <SearchIcon />
-            <InputBase />
+            <div className={classes.searchIcon}>
+              <SearchIcon/>
+            </div>
+            <InputBase placeholder="Search..." className={classes.input}/>
           </div>
         </Toolbar>
       </AppBar>
