3272bc9e0028488b5ae5d2dd4c74e0680f3829c0
Kevin Luo <kvn.luo@gmail.com>
2020-11-17 11:10:37 -0500

    useAuthContext hook skeleton

--- client/src/utils/useAuthContext.js
+++ client/src/utils/useAuthContext.js
@@ -1,3 +1,21 @@
 import React, { useContext, useReducer } from 'react';
 import jwtDecode from 'jwt-decode';
 
+const AuthContext = React.createContext;
+
+function AuthProvider(props) {
+
+
+  return(
+    <AuthContext.Provider 
+
+      {...props}
+    />
+  )
+}
+
+function useAuthContext() {
+  return useContext(AuthContext);
+}
+
+export { useAuthContext, AuthProvider }
