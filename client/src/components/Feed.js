f905e2f9db7e79cd3ee7d6f5e71a45790eb55eff
Kevin Luo <kvn.luo@gmail.com>
2020-11-17 00:24:20 -0500

    display posts from db

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { useQuery } from '@apollo/client';
 import { Grid, Paper, Avatar, Typography, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { FaRegComment, FaRegHeart, FaRegImage } from 'react-icons/fa';
+import { FaRegComment, FaRegHeart } from 'react-icons/fa';
 import { FiImage } from 'react-icons/fi';
 import { AiOutlineDelete } from 'react-icons/ai';
+
+import { FETCH_PINGS_QUERY } from '../utils/graphql';
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -19,136 +22,46 @@ const useStyles = makeStyles(theme => ({
 
 export default function Feed() {
   const classes = useStyles();
-  const message = "posts go here where they belongposts go here where they belongposts go here where they belongposts go here where they belongposts go here where they belongposts go here where they belongposts go here where they belongposts go here where they belongposts go here where they belongposts go here where they belongposts go here where they belongposts go here where they belong"
+  const { loading, data } = useQuery(FETCH_PINGS_QUERY);
+
   return (
     <>
-      <Paper className={classes.paper}>
-        <Grid container wrap="nowrap" spacing={2} alignItems="center">
-          <Grid item >
-            <Avatar className={classes.pic}>Pic</Avatar>
-          </Grid>
-          <Grid item >
-            <FiImage size={32}/>
-          </Grid>
-          <Grid item xs >
-            <Typography>{message.substring(0,250)}</Typography>
-          </Grid>
-          <Grid item xs={2} container wrap="nowrap">
-            <Grid item>
-              <IconButton>
-                <FaRegHeart />
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <IconButton>
-                <FaRegComment />
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <IconButton>
-                <AiOutlineDelete size={26}/>
-              </IconButton>
-            </Grid>
-          </Grid>
-        </Grid>
-
-      </Paper>
-
-      <Paper className={classes.paper}>
-        <Grid container wrap="nowrap" spacing={2} alignItems="center">
-          <Grid item >
-            <Avatar className={classes.pic}>Pic</Avatar>
-          </Grid>
-          <Grid item >
-            <FiImage size={32}/>
-          </Grid>
-          <Grid item xs >
-            <Typography>{message.substring(0,200)}</Typography>
-          </Grid>
-          <Grid item xs={2} container wrap="nowrap">
-            <Grid item>
-              <IconButton>
-                <FaRegHeart />
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <IconButton>
-                <FaRegComment />
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <IconButton>
-                <AiOutlineDelete size={26}/>
-              </IconButton>
-            </Grid>
-          </Grid>
-        </Grid>
-
-      </Paper>
-
-      <Paper className={classes.paper}>
-        <Grid container wrap="nowrap" spacing={2} alignItems="center">
-          <Grid item >
-            <Avatar className={classes.pic}>Pic</Avatar>
-          </Grid>
-          <Grid item >
-            <FiImage size={32}/>
-          </Grid>
-          <Grid item xs >
-            <Typography>{message.substring(0,150)}</Typography>
-          </Grid>
-          <Grid item xs={2} container wrap="nowrap">
-            <Grid item>
-              <IconButton>
-                <FaRegHeart />
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <IconButton>
-                <FaRegComment />
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <IconButton>
-                <AiOutlineDelete size={26}/>
-              </IconButton>
-            </Grid>
-          </Grid>
-        </Grid>
-
-      </Paper>
-
-      <Paper className={classes.paper}>
-        <Grid container wrap="nowrap" spacing={2} alignItems="center">
-          <Grid item >
-            <Avatar className={classes.pic}>Pic</Avatar>
-          </Grid>
-          <Grid item >
-            <FiImage size={32}/>
-          </Grid>
-          <Grid item xs >
-            <Typography>{message.substring(0,100)}</Typography>
-          </Grid>
-          <Grid item xs={2} container wrap="nowrap">
-            <Grid item>
-              <IconButton>
-                <FaRegHeart />
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <IconButton>
-                <FaRegComment />
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <IconButton>
-                <AiOutlineDelete size={26}/>
-              </IconButton>
-            </Grid>
-          </Grid>
-        </Grid>
-
-      </Paper>
+      { loading || (
+        data.getPings && data.getPings.map(ping => {
+          return(
+            <Paper className={classes.paper}>
+              <Grid container wrap="nowrap" spacing={2} alignItems="center">
+                <Grid item >
+                  <Avatar className={classes.pic}>Pic</Avatar>
+                </Grid>
+                <Grid item >
+                  <FiImage size={32}/>
+                </Grid>
+                <Grid item xs >
+                  <Typography>{ping.body}</Typography>
+                </Grid>
+                <Grid item xs={2} container wrap="nowrap">
+                  <Grid item>
+                    <IconButton>
+                      <FaRegHeart />
+                    </IconButton>
+                  </Grid>
+                  <Grid item>
+                    <IconButton>
+                      <FaRegComment />
+                    </IconButton>
+                  </Grid>
+                  <Grid item>
+                    <IconButton>
+                      <AiOutlineDelete size={26}/>
+                    </IconButton>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Paper>
+          )
+        })
+      )}
     </>
   )
 }
