88a652cd6e46d821735a3caa5e2fa055e7ed1617
Kevin Luo <kvn.luo@gmail.com>
2020-11-17 21:55:08 -0500

    create ping styling

--- client/src/components/NewPing.js
+++ client/src/components/NewPing.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Paper, Button, TextField } from '@material-ui/core';
+import { Paper, Button, ButtonGroup, TextField, Grid, Icon } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles'
+import SendIcon from '@material-ui/icons/Send'
+import CloudUploadIcon from '@material-ui/icons/CloudUpload'
 
 import { CREATE_PING } from '../utils/graphql';
 
@@ -8,7 +10,6 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     margin: theme.spacing(2, 1),
     padding: theme.spacing(2, 2),
-    display: 'flex',
   }
 }));
 
@@ -16,9 +17,19 @@ export default function NewPing() {
   const classes = useStyles();
   return (
     <Paper className={classes.paper}>
-      <form>
-        <TextField />
-      </form>
+      <Grid container alignItems="center" >
+        <form style={{display: 'flex'}}>
+          <Grid item xs={10}>
+            <TextField rowsMax="3" multiline fullWidth/>
+          </Grid>
+          <Grid item xs={2} align>
+            <ButtonGroup size="small">
+              <Button endIcon={<CloudUploadIcon />}>Upload</Button>
+              <Button endIcon={<SendIcon />}>Ping</Button>
+            </ButtonGroup>
+          </Grid>
+        </form>
+      </Grid>
     </Paper>
   )
 }
