ca3da567f3a4bf07e6bcdb2e80c36da56a850203
Kevin Luo <kvn.luo@gmail.com>
2020-11-17 23:49:25 -0500

    add like toggle frontend

--- client/src/components/SupportPing.js
+++ client/src/components/SupportPing.js
@@ -1,37 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useMutation } from '@apollo/client';
 import { IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { FaRegHeart } from 'react-icons/fa';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
 
 import { SUPPORT_PING } from '../utils/graphql';
 
 const useStyles = makeStyles(themes => ({
-
+  icon: {
+    color: 'red',
+  }
 }))
 
-export default function SupportPing({user, ping}) {
+export default function SupportPing({user, ping: { id, support, supportCount }}) {
   const classes = useStyles();
+  const [supported, setSupported] = useState(false);
 
-  const [support] = useMutation(SUPPORT_PING, {
+  useEffect(() => {
+    if (user) {
+      if (support.find(supporter => user.username === supporter.username)) {
+        setSupported(true);
+      } else {
+        setSupported(false)
+      }
+    }  
+  }, [support, user])
+
+  const [supportMutation] = useMutation(SUPPORT_PING, {
     variables: {
-      pingId: ping.id
+      pingId: id
     },
     onError(err) {
       console.log(err);
-    },
-    update(_, result) {
-      console.log(result);
     }
   })
 
-  const toggleSupport = () => {
-    console.log(user, ping)
-  }
-
   return (
-    <IconButton onClick={toggleSupport}>
-      <FaRegHeart style={{ color: "red" }} size={15} />
+    <IconButton onClick={supportMutation}>
+      {supported ? (
+        <FaHeart className={classes.icon} size={15} />
+      ) : (
+        <FaRegHeart className={classes.icon} size={15} />
+      )}
     </IconButton>
   )
 }
