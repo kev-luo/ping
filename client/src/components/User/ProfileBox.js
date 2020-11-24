3804d169c4e1a0eef9c113f826b971571213dd9b
Kevin Luo <kvn.luo@gmail.com>
2020-11-24 10:29:59 -0500

    consolidate profile box display

--- client/src/components/User/ProfileBox.js
+++ client/src/components/User/ProfileBox.js
@@ -5,6 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { RiUserSettingsLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 
+import Loading from "../Loading";
 import Actions from "../../utils/dashboardActions";
 import { useAuthContext } from "../../utils/useAuthContext";
 import { useDashboardContext } from "../../utils/useDashboardContext";
@@ -15,49 +16,36 @@ export default function ProfileBox() {
   const { user } = useAuthContext();
   const [state, dispatch] = useDashboardContext();
 
-  const currentUser = state.selectedUser || user;
-
   useEffect(() => {
     user && dispatch({ type: Actions.SELECT_USER, payload: user });
   }, []);
 
   const { loading, data } = useQuery(FETCH_USER_QUERY, {
-    variables: { userId: currentUser.id },
+    skip: !state.selectedUser,
+    variables: { userId: state.selectedUser?.id },
   });
 
   return (
     <div className={classes.root}>
-      {loading ||
-        (state.selectedUser ? (
-          <>
-            <Typography
-              variant="h4"
-              className={classes.handle}
-            >{`@${state.selectedUser.username}`}</Typography>
-            <Link to={`/user/supported/${data.getUser.id}`}>
-              <Button>Supported Pings</Button>
-            </Link>
-            <Link to={`/user/pinged/${data.getUser.id}`}>
-              <Button>Posted Pings</Button>
-            </Link>
-          </>
-        ) : (
-          <>
-            <Link to="settings">
-              <RiUserSettingsLine style={{ float: "right" }} />
-            </Link>
-            <Typography
-              variant="h4"
-              className={classes.handle}
-            >{`@${data.getUser.username}`}</Typography>
-            <Link to={`/user/supported/${data.getUser.id}`}>
-              <Button>Supported Pings</Button>
-            </Link>
-            <Link to={`/user/pinged/${data.getUser.id}`}>
-              <Button>Posted Pings</Button>
-            </Link>
-          </>
-        ))}
+      {data ? (
+        <>
+          <Link to="settings">
+            <RiUserSettingsLine style={{ float: "right" }} />
+          </Link>
+          <Typography
+            variant="h4"
+            className={classes.handle}
+          >{`@${data.getUser.username}`}</Typography>
+          <Link to={`/user/supported/${data.getUser.id}`}>
+            <Button>Supported Pings</Button>
+          </Link>
+          <Link to={`/user/pinged/${data.getUser.id}`}>
+            <Button>Posted Pings</Button>
+          </Link>
+        </>
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 }
