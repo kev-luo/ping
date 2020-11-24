933f9f7f63f38ac6b340ad7ba2e1c3e48e30237c
Kevin Luo <kvn.luo@gmail.com>
2020-11-24 10:41:10 -0500

    query to fetch user data put in user container instead of profile box

--- client/src/components/User/ProfileBox.js
+++ client/src/components/User/ProfileBox.js
@@ -1,29 +1,17 @@
-import React, { useEffect } from "react";
-import { useQuery } from "@apollo/client";
+import React from "react";
 import { Typography, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { RiUserSettingsLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 
 import Loading from "../Loading";
-import Actions from "../../utils/dashboardActions";
 import { useAuthContext } from "../../utils/useAuthContext";
 import { useDashboardContext } from "../../utils/useDashboardContext";
-import { FETCH_USER_QUERY } from "../../utils/graphql";
 
-export default function ProfileBox() {
+export default function ProfileBox({ userData }) {
   const classes = useStyles();
   const { user } = useAuthContext();
-  const [state, dispatch] = useDashboardContext();
-
-  useEffect(() => {
-    user && dispatch({ type: Actions.SELECT_USER, payload: user });
-  }, []);
-
-  const { loading, data } = useQuery(FETCH_USER_QUERY, {
-    skip: !state.selectedUser,
-    variables: { userId: state.selectedUser?.id },
-  });
+  const [state] = useDashboardContext();
 
   function editProfile() {
     if (user.id === state.selectedUser.id) {
@@ -37,17 +25,17 @@ export default function ProfileBox() {
 
   return (
     <div className={classes.root}>
-      {data ? (
+      {userData ? (
         <>
           {editProfile()}
           <Typography
             variant="h4"
             className={classes.handle}
-          >{`@${data.getUser.username}`}</Typography>
-          <Link to={`/user/supported/${data.getUser.id}`}>
+          >{`@${userData.username}`}</Typography>
+          <Link to={`/user/supported/${userData.id}`}>
             <Button>Supported Pings</Button>
           </Link>
-          <Link to={`/user/pinged/${data.getUser.id}`}>
+          <Link to={`/user/pinged/${userData.id}`}>
             <Button>Posted Pings</Button>
           </Link>
         </>
