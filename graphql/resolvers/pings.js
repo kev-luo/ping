e491b297db0a4605458b7546a3caec0af290be62
JD Martinez <focus4ursoul@gmail.com>
2020-11-27 21:07:57 -0800

    increase range for testing

--- graphql/resolvers/pings.js
+++ graphql/resolvers/pings.js
@@ -25,7 +25,7 @@ module.exports = {
       try {
         const pings = await Ping.find({location: {
           $near: {
-           $maxDistance: 100,
+           $maxDistance: 10000000,
            $geometry: {
             type: "Point",
             coordinates: [long, latt]
